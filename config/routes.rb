f749a9362e39f3bcaec233a332843ba503a69c4f
Emmanuel Asante <emmanuel@Emmanuels-MacBook-Pro.local>
2019-05-16 06:03:47 -0600

    Add product show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "products#index"
+
+  get '/products/:id', to: 'products#show', as: 'product'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
