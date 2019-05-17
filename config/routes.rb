f4e552a72aec60d3f1feff26773c141257d68fee
Emmanuel Asante <emmanuel@Emmanuels-MacBook-Pro.local>
2019-05-17 06:42:40 -0600

    Use resourceful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root to: "products#index"
 
-
-  get '/products',          to: 'products#index'
-  get '/products/new',      to: 'products#new', as: 'new_product'
-  get '/products/:id',      to: 'products#show', as: 'product'
-  post '/products',         to: 'products#create'
-  get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
-  patch '/products/:id',    to: 'products#update'
-  delete '/products/:id',   to: 'products#destroy'
+  resources :products
+  # get '/products',          to: 'products#index'
+  # get '/products/new',      to: 'products#new', as: 'new_product'
+  # get '/products/:id',      to: 'products#show', as: 'product'
+  # post '/products',         to: 'products#create'
+  # get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
+  # patch '/products/:id',    to: 'products#update'
+  # delete '/products/:id',   to: 'products#destroy'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
