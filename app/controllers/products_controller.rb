9dbcb315b635295ae6d1cab7327b9a508d0c13ac
Emmanuel Asante <emmanuel@Emmanuels-MacBook-Pro.local>
2019-05-17 04:29:05 -0600

    Post edited data to server

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,10 @@ class ProductsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
   private
 
   def product_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root to: "products#index"
 
 
-  get '/products',     to: 'products#index'
-  get '/products/new', to: 'products#new', as: 'new_product'
-  get '/products/:id', to: 'products#show', as: 'product'
-  post '/products',    to: 'products#create'
+  get '/products',          to: 'products#index'
+  get '/products/new',      to: 'products#new', as: 'new_product'
+  get '/products/:id',      to: 'products#show', as: 'product'
+  post '/products',         to: 'products#create'
+  get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
