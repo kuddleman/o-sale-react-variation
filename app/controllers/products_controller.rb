7c13f362fe1e1a43b5580c873216e9294a18d871
Emmanuel Asante <emmanuel@Emmanuels-MacBook-Pro.local>
2019-05-17 06:35:56 -0600

    Add flash messages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,8 +15,10 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
+      flash[:notice] = "Product has been saved"
       redirect_to root_path
     else
+      flash.now[:alert] = "Product has not been saved"
       render :new
     end
   end
@@ -26,8 +28,10 @@ class ProductsController < ApplicationController
 
   def update
     if @product.update(product_params)
+      flash[:notice] = "Product has been updated"
       redirect_to root_path
     else
+      flash.now[:alert] = "Product has not been updated"
       render :edit
     end
   end
