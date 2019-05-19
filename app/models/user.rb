d8194d84d8173a089a50d4bb1433bbd70c2f6c91
Emmanuel Asante <emmanuel@Emmanuels-MacBook-Pro.local>
2019-05-19 05:30:37 -0600

    Write email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, presence: true
-  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, 
+    presence: true, 
+    uniqueness: { case_sensitive: false },
+    format: { 
+      with: /\A[A-Z0-9#-_~!$&'()*+,;=:.]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i 
+    }
   
   has_secure_password
 end
