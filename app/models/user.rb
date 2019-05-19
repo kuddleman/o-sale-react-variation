c9a75d9fd59e5904c47f9275cfdbaead237c0962
Emmanuel Asante <emmanuel@Emmanuels-MacBook-Pro.local>
2019-05-19 04:33:29 -0600

    Validates user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  
   has_secure_password
 end
