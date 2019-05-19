67fea17901aa61575cc013e9aff033698b6f95e7
Emmanuel Asante <emmanuel@Emmanuels-MacBook-Pro.local>
2019-05-19 08:41:31 -0600

    Sign a user up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Signed up successfully"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Sign up failed"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
