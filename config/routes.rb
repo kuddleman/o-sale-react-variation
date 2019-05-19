17b7ec1ccda5d599997d5c8a290f5387ae32464e
Emmanuel Asante <emmanuel@Emmanuels-MacBook-Pro.local>
2019-05-19 10:35:22 -0600

    Create signin routes, controller and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new', to: 'users#new', as: 'new_user'
-  get '/signup',   to: 'users#new'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new',   to: 'users#new', as: 'new_user'
+  get '/signup',     to: 'users#new'
+  get '/signin',     to: 'sessions#new'
+  post '/signin',    to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy', as: 'session'
   
   resources :users, only: [:create]
 
