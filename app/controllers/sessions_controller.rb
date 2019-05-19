e4f240210a2e9b37f488a95606d73f86beb850f2
Emmanuel Asante <emmanuel@Emmanuels-MacBook-Pro.local>
2019-05-19 11:34:34 -0600

    Prevent multiple signin attempts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,18 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to root_path if current_user
   end
 
   def create
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      cookies.signed[:user_id] = user.id
+      flash[:notice] = "Sign in successful"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Invalid email/password combination"
+      render :new
+    end
   end
 
   def destroy
