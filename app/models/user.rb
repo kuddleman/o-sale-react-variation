bbbb1aa9fe92d63d64d6c075a322d2d5ba0abb1b
Emmanuel Asante <emmanuel@Emmanuels-MacBook-Pro.local>
2019-05-19 18:42:04 -0600

    Show user validation errors in view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name, presence: true
   validates :email, 
     presence: true, 
     uniqueness: { case_sensitive: false },
