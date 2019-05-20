79f262870ab6679b67640741a007a150a4f0d1a7
Emmanuel Asante <emmanuel@Emmanuels-MacBook-Pro.local>
2019-05-20 13:56:35 -0600

    Create Comments create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :require_signin
+  before_action :set_product
+
   def create
+    @comment = @product.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      flash[:notice] = "Comment has been created"
+      redirect_to @product
+    else
+      flash.now[:alert] = "Comment has not been created"
+      render 'products/show'
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.where(id: params[:product_id]).first
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
