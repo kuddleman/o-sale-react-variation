9eaf0ef1082ad8d0a10cd15456b78d2eae454da2
Kuddleman <edl1155@gmail.com>
2020-03-24 12:29:42 -0700

    create api's comments controller

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::CommentsController < ApplicationController
+  before_action :require_signin, only: [ :create ]
+  before_action :set_product
+
+  def index
+    @comments = @product.comments
+  end
+
+  def create
+    @comment = @product.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      flash[:notice] = "Comment has been created"
+      redirect_to @product
+    else
+      @comments = @product.comments
+      flash.now[:alert] = "Comment has not been created"
+      render 'products/show'
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.where(id: params[:product_id]).first
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
---
+++ app/views/api/v1/comments/index.json.jbuilder
@@ -0,0 +1,10 @@
+json.comments (@comments) do |comment|
+  json.id = comment.id
+  json.body = comment.body
+  json.created_at = comment.created_at
+  jason.product_id = comment.product_id
+  json.user do
+    json.fullname = comment.user.fullname
+  end
+end  
+
